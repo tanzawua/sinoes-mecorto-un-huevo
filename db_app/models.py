b3dd645800ef4e70ce4b571508ebcdba40491a9c
tanzawua2 <vm9702019@gmail.com>
2022-10-27 17:46:43 -0500

    soy e

--- db_app/models.py
+++ db_app/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Persona(models.Model):
-    referencia = models.CharField(max_length=200)
-    nombre = models.CharField(max_length=200)
-    precio = models.IntegerField()
-    nuevo = models.BooleanField()
-    descripcion = models.TextField()
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+ 
+    
--- db_app/serializers.py
+++ db_app/serializers.py
@@ -6,5 +6,4 @@ from rest_framework import serializers
 class PersonaSerializers(serializers.ModelSerializer):
     class Meta:
         model = Persona
-        fields = ('referencia','nombre','descripcion','nuevo','precio')
-        read_only_fields = ('fecha',)
+        fields = '__all__'
