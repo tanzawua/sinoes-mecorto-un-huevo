fec6bf9c7d07b10f7e07736606e52a65542d0d8b
tanzawua2 <vm9702019@gmail.com>
2022-11-09 16:01:34 -0500

    master

--- db_app/models.py
+++ db_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Servicio(models.Model):
     nombre = models.CharField(max_length=200)
-    apellido = models.CharField(max_length=200)
+    apellido = models
     referencia = models.CharField(max_length=200)
     precio = models.IntegerField()
     nuevo = models.BooleanField()
